6d2d993e2f3567c72e71239a622148d8de951ce3
R Reyes <117328790+reyesrj2012@users.noreply.github.com>
2024-07-24 19:09:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 const quizData + [
   {
     question: "Are you self-displined?"
-    options: [yes, no}
+    options: [yes, no]
     answer: "Dog"
 },
 { 
   question: "Are you extroverted?"
-  options: 
+  options: [yes, no]
+  answer: "Dog"
+}
+{
+question: "Do you love the outdoors?"
+options: [yes, no]
+answer: "Dog"
+}
+
+const questionElement= document.getElementById
 console.log('hello world! Project initialized')
