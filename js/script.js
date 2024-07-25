495870eb20083b2c006343c28ab508548fbbdd71
Schooler99 <sophiaschooler@gmail.com>
2024-07-24 19:22:05 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-const quizData + [
+const quizData = [
   {
     question: "Are you self-displined?"
-    options: [yes, no]
+    options: [yes, no],
     answer: "Dog"
-},
-{ 
-  question: "Are you extroverted?"
-  options: [yes, no]
-  answer: "Dog"
-}
-{
-question: "Do you love the outdoors?"
-options: [yes, no]
-answer: "Dog"
-}
-
-const questionElement= document.getElementById
-console.log('hello world! Project initialized')
+  },
+  { 
+    question: "Are you extroverted?"
+    options: [yes, no]
+    answer: "dog"
+  }, 
+  {
+    question: "Do you love the outdoors?"
+    options: [yes, no}
+    answer: "Dog"
+  },
+  {
+    question: "Are you curious? Creative?"
+    options: [yes, no}
+    answer: "Cat"
+  },
+  {
+    question: "Are you a little neurotic? Easily Stressed?"
+    options: [yes, no}
+    answer: "Cat"
+  },
+  {
+    question: "Do you prefer to spend time on your own?"
+    options: [yes, no}
+    answer: "Cat"
+  },
+  {
+    question: "Are you independent? Analytical?"
+    options: [yes, no}
+    answer: "Reptile"
+  },
+  {
+    question: "Shows interest in science/nature?"
+    options: [yes, no}
+    answer: "Reptile"
+  },
+  {
+    question: "Prefer a pet without fur?"
+    options: [yes, no}
+    answer: "Reptile"
+  },
+  
+];
+  console.log('hello world! Project initialized')
