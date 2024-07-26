e11cdef42333b3266ab12bc3b76bb77e2c5bf800
R Reyes <117328790+reyesrj2012@users.noreply.github.com>
2024-07-25 19:18:35 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-  const quizData = [
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
+const quizData = [
   {
     question: "Are you self-displined?"
     options: [yes, no],
