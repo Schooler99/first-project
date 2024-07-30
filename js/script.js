da9e821e32a49bd86c0bd493505e414588d0370e
Schooler99 <sophiaschooler@gmail.com>
2024-07-29 20:46:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const showButton = document.querySelector("dialog + button");
   const closeButton = document.querySelector("dialog button");
 
-  // "Show the dialog" button opens the dialog modally
   showButton.addEventListener("click", () => {
     dialog.showModal();
   });
 
-  // "Close" button closes the dialog
   closeButton.addEventListener("click", () => {
     dialog.close();
   });
